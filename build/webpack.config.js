a293dfcf0575bafb615bf88835dc9211974bd627
raind33 <2331336537@qq.com>
2019-07-08 15:26:50 +0800

    初步搭建

---
+++ build/webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  devtool:
+    process.env.NODE_ENV === 'production'
+      ? 'cheap-module-source-map'
+      : 'cheap-module-eval-source-map',
+  entry: {
+    main: './src/index.ts'
+  },
+  output: {
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  devServer: {
+    contentBase: './dist',
+    port: 3000,
+    hot: true,
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template/index.html'
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['dist']
+    })
+  ]
+}
