cd06062b8c58bf89d6b449d67bab87668f335dd7
raind33 <2331336537@qq.com>
2019-07-16 14:16:56 +0800

    no

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-interface Vegetables {
-  color?: string
-  readonly type: string
-  [prop: string]: any
+const getProp = <T, K extends keyof T>(object: T, propName: K) => {
+  return object[propName]
 }
+const obj = { a: 'aa', b: 'bb' }
+getProp(obj, 'c') // 类型“"c"”的参数不能赋给类型“"a" | "b"”的参数
